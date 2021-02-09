456331e8341db88c965cf0611aeacf730e245912
unknown <domgonzales14@gmail.com>
2021-02-09 13:01:44 -0800

    update

--- static/js/hitwords.js
+++ static/js/hitwords.js
@@ -1,11 +1,11 @@
-d3.csv("../datasets/hit_flop_words.csv").then(function(Data){
+d3.csv("/resources/datasets/hit_flop_words.csv").then(function(Data){
 
     var tbody = d3.select("tbody")
     
-    // Data.forEach(function(data) {
-    //     console.log(data)
+    Data.forEach(function(data) {
+        console.log(data)
         
-    //   });
+      });
     
     // Step 2:  Use d3 to append one table row `tr` for each object
     Data.forEach(function(Data) {
